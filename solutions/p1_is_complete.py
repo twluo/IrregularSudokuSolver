f318d22f69ead044f176be185518e450ee1dc0f0
Elton Chen <elc036@ucsd.edu>
2016-05-08 18:44:57 -0700

    Initial Commit

---
+++ solutions/p1_is_complete.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+
+def is_complete(csp):
+    """Returns True when the CSP assignment is complete, i.e. all of the variables in the CSP have values assigned."""
+
+    # Hint: The list of all variables for the CSP can be obtained by csp.variables.
+    # Also, if the variable is assigned, variable.is assigned() will be True.
+    # (Note that this can happen either by explicit assignment using variable.assign(value),
+    # or when the domain of the variable has been reduced to a single value.)
+
+    # TODO implement this
+    pass
---
+++ solutions/p2_is_consistent.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+
+def is_consistent(csp, variable, value):
+    """Returns True when the variable assignment to value is consistent, i.e. it does not violate any of the constraints
+    associated with the given variable for the variables that have values assigned.
+
+    For example, if the current variable is X and its neighbors are Y and Z (there are constraints (X,Y) and (X,Z)
+    in csp.constraints), and the current assignment as Y=y, we want to check if the value x we want to assign to X
+    violates the constraint c(x,y).  This method does not check c(x,Z), because Z is not yet assigned."""
+
+    # TODO implement this
+    pass
