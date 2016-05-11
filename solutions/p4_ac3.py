2bde3bf9827792045e0ef63b90fe3fc60686d77f
Tony Luo <tonywluo@gmail.com>
2016-05-11 08:38:01 -0700

    Part 4 passes all testcases

--- solutions/p4_ac3.py
+++ solutions/p4_ac3.py
@@ -15,10 +15,27 @@ def ac3(csp, arcs=None):
     This method returns True if the arc consistency check succeeds, and False otherwise."""
 
     queue_arcs = deque(arcs if arcs is not None else csp.constraints.arcs())
-
+    while queue_arcs:
+        xi, xj = queue_arcs.pop()
+        if revise(csp, xi, xj):
+            if not xi.domain:
+                return False
+            else:
+                for xk in csp.constraints[xi]:
+                    queue_arcs.append((xi, xk.var2))
+    return True
     # TODO implement this
-    pass
 
 def revise(csp, xi, xj):
     # You may additionally want to implement the 'revise' method.
-    pass
+    change = False
+    for vali in xi.domain:
+        for contraint in csp.constraints[xi, xj]:
+            for valj in xj.domain:
+                if contraint.is_satisfied(vali, valj):
+                    break
+            else:
+                xi.domain.remove(vali)
+                change = True
+    return change
+
